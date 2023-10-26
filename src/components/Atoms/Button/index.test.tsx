2432758f3ca4d494c50005efbf29ef5047e3e4e0
ricardo <ricardo.br.pi@gmail.com>
2023-10-26 14:39:06 -0300

    feat(test): add test to project

---
+++ src/components/Atoms/Button/index.test.tsx
@@ -0,0 +1,7 @@
+import renderer from "react-test-renderer";
+import Button from "./index";
+
+test("Link renders correctly", () => {
+  const tree = renderer.create(<Button type="button" >Click</Button>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
