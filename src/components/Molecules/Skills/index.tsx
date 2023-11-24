6ba1ea780bcf9144589409dbbef4245cf53a8d91
ricardo <ricardo.br.pi@gmail.com>
2023-11-24 18:07:13 -0300

    feat(skills): adding skills to the component

--- src/components/Molecules/Skills/index.tsx
+++ src/components/Molecules/Skills/index.tsx
@@ -1,13 +1,45 @@
-import Text from "../../Atoms/Text"
-import { SkillStyle, BoxText } from "./style"
+import Image from "next/image";
+import Text from "../../Atoms/Text";
+import { SkillStyle, BoxText, BoxSkillSItems, BoxSkill } from "./style";
+import { skillsProps, } from "./types";
 
-export default function Skills() {
+export default function Skills({ skillItens }: skillsProps) {
+  skillItens = [
+    "Next.js",
+    "React.js",
+    "JavaScript",
+    "Storybook",
+    "Firebase",
+    "Styled-components",
+    "HTML",
+    "CSS",
+    "Jest",
+    "SASS",
+    "Cypress.io",
+    "Bootstrap",
+    "JQuery",
+    "Prettier",
+    "ESLint",
+  ];
+
   return (
     <SkillStyle>
       <BoxText>
-          <Text color="white" fontSize="20px">Skills</Text>
-          <Text color="white" fontSize="16px">The skills, tools and technologies I am good at:</Text>
+        <Text color="white" fontSize="20px">Skills</Text>
+        <Text color="white" fontSize="16px">The skills, tools and technologies I am good at:</Text>
       </BoxText>
+      <BoxSkillSItems>
+        {
+          skillItens.map((skillItens, index) => {
+            return (
+              <BoxSkill key={index}>
+                <Image width={40} height={40} src={"/img/"+skillItens+".png"} alt="" />
+                <Text color="white" fontSize="16px">{skillItens}</Text>
+              </BoxSkill>
+            )
+          })
+        }
+     </BoxSkillSItems>
     </SkillStyle>
   )
 }
--- src/components/Molecules/Skills/style.ts
+++ src/components/Molecules/Skills/style.ts
@@ -4,7 +4,14 @@ import screens from "../../../tokens/screen";
 export const SkillStyle = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: center;
+  height: 700px;
+
+  @media (min-width: ${screens.large}) {
+    height: 400px;
+  }
+
 `;
 
 export const BoxText = styled.div`
@@ -12,8 +19,33 @@ export const BoxText = styled.div`
   justify-content: space-between;
   flex-direction: column;
   align-items: center;
-  height: 60px;
+  height: 100px;
 
-  @media (min-width: ${screens.small}) {
+`
+export const BoxSkillSItems = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  height: 800px;
+  width: 300px;
+  justify-content: space-between;
+
+  @media (min-width: ${screens.large}) {
+    width: 640px;
+    height: 300px;
+
+  }
+`
+
+export const BoxSkill = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 78px;
+  height: 96px;
+
+  @media (min-width: ${screens.large}) {
+
   }
 `
