cbd8a566d2c1491d353f35367b230a75454678ce
ricardo <ricardo.br.pi@gmail.com>
2023-11-24 19:08:17 -0300

    feat(pages): add required property by skills component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,29 @@ import Header from "../src/components/Molecules/Header";
 import Skills from "../src/components/Molecules/Skills";
 
 export default function Home() {
+  const skillItens = [
+    "Next.js",
+    "React.js",
+    "JavaScript",
+    "Storybook",
+    "Firebase",
+    "Styled-components",
+    "HTML",
+    "CSS",
+    "Jest",
+    "SASS",
+    "Cypress.io",
+    "Bootstrap",
+    "JQuery",
+    "Prettier",
+    "ESLint",
+
+  ];
   return (
     <>
       <Menu />
       <Header />
-      <Skills />
+      <Skills skillItens={skillItens} />
     </>
   );
 }
