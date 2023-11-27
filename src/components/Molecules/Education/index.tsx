aaa14ed8df46da0ad7a56937b86ad6181dc0fbde
ricardo <ricardo.br.pi@gmail.com>
2023-11-27 09:31:00 -0300

    feat(education): add college information

--- src/components/Molecules/Education/index.tsx
+++ src/components/Molecules/Education/index.tsx
@@ -1,9 +1,18 @@
+import Text from "../../Atoms/Text";
+import { EducationStyle, BoxText, BoxTextInformation } from "./style";
+
 export default function Education() {
   return (
-    <div>
-      <div>
-        <h2>Hello</h2>
-      </div>
-    </div>
+    <EducationStyle>
+      <BoxText>
+        <Text color="white" fontSize="20px">Education</Text>
+        <Text color="white" fontSize="16px">These are my qualifications</Text>
+      </BoxText>
+      <BoxTextInformation>
+        <Text color="#66BEF5" fontSize="16px">Uninassau</Text>
+        <Text color="white" fontSize="16px">Analise e desenvolvimento de sistemas</Text>
+        <Text color="white" fontSize="16px">State: Cursando</Text>
+      </BoxTextInformation>
+    </EducationStyle>
   )
 }
