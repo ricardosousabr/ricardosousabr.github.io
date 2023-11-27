40242204889e503600f212d0953dee2e06231033
ricardo <ricardo.br.pi@gmail.com>
2023-11-27 09:58:48 -0300

    feat(footer): create component footer

---
+++ src/components/Molecules/Footer/index.tsx
@@ -0,0 +1,10 @@
+import Text from "../../Atoms/Text"
+
+export default function Footer() {
+  return (
+    <div>
+      <Text color="white" fontSize="20px">Education</Text>
+      <Text color="white" fontSize="16px">What’s next? Feel free to reach out to me if you're looking for a developer, have a query, or simply want to connect.</Text>
+    </div>
+  )
+}
