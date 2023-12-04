ab25d877194ace9b880eb5cf1b33fce85651bfb5
ricardo <ricardo.br.pi@gmail.com>
2023-12-04 09:06:08 -0300

    feat(project): create project component

---
+++ src/components/Molecules/Project/index.tsx
@@ -0,0 +1,10 @@
+import Text from "../../Atoms/Text"
+import { ProjectStyle } from "./style"
+
+export default function Project() {
+  return (
+    <ProjectStyle>
+      <Text color="white" fontSize="20px">Project</Text>
+    </ProjectStyle>
+  )
+}
---
+++ src/components/Molecules/Project/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ProjectStyle = styled.div`
+  background: #111827;
+  display: flex;
+  justify-content: center;
+
+`
