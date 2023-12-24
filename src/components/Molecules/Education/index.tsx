4bb939efe648cc8df53480ce6a662e4ac33568b3
ricardo <ricardo.br.pi@gmail.com>
2023-12-24 00:37:56 -0300

    feat(education): include container component

--- src/components/Molecules/Education/index.tsx
+++ src/components/Molecules/Education/index.tsx
@@ -1,22 +1,27 @@
 import Text from "../../Atoms/Text";
-import { EducationStyle, BoxText, BoxTitle, BoxTextInformation } from "./style";
+import Container from "../../Atoms/Container";
+import { EducationStyle, Wrapper, BoxText, BoxTitle, BoxTextInformation } from "./style";
 
 export default function Education() {
   return (
-    <EducationStyle>
-      <BoxText>
-        <BoxTitle>
-          <Text color="white" fontSize="20px">Education</Text>
-        </BoxTitle>
-        <div>
-          <Text color="white" fontSize="16px">These are my qualifications</Text>
-        </div>
-      </BoxText>
-      <BoxTextInformation>
-        <Text color="#66BEF5" fontSize="16px">Uninassau</Text>
-        <Text color="white" fontSize="16px">Analise e desenvolvimento de sistemas</Text>
-        <Text color="white" fontSize="16px">State: Cursando</Text>
-      </BoxTextInformation>
-    </EducationStyle>
+    <Wrapper>
+      <Container>
+        <EducationStyle>
+          <BoxText>
+            <BoxTitle>
+              <Text color="white" fontSize="20px">Education</Text>
+            </BoxTitle>
+            <div>
+              <Text color="white" fontSize="16px">These are my qualifications</Text>
+            </div>
+          </BoxText>
+          <BoxTextInformation>
+            <Text color="#66BEF5" fontSize="16px">Uninassau</Text>
+            <Text color="white" fontSize="16px">Analise e desenvolvimento de sistemas</Text>
+            <Text color="white" fontSize="16px">State: Cursando</Text>
+          </BoxTextInformation>
+        </EducationStyle>
+      </Container>
+    </Wrapper>
   )
 }
