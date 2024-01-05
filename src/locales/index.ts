f5b0d9b0b40a7a4abb308b2f9af1401d6a522337
ricardo <ricardo.br.pi@gmail.com>
2024-01-05 15:21:28 -0300

    feat: add typing information file

---
+++ src/locales/index.ts
@@ -0,0 +1,112 @@
+const locale = {
+  "pt-br": {
+    header: {
+      title: "Ricardo é desenvolvedor front-end",
+      subtitle: "Ele cria sites responsivos onde a tecnologia encontra a criatividade",
+      motovationalPhrase: "“O importante é não parar de questionar; a curiosidade tem sua própria razão de existir” - Albert Einstein",
+      image: "/img/Profile.png",
+    },
+    skills: {
+      title: "Habilidades",
+      subtitle: "As habilidades, ferramentas e tecnologias nas quais sou bom:",
+      technologies: [
+        {
+          name: "Next.js",
+          src: "/img/Next.js.png",
+        },
+        {
+          name: "React.js",
+          src: "/img/React.js.png",
+        },
+        {
+          name: "JavaScript",
+          src: "/img/JavaScript.png",
+        },
+        {
+          name: "Storybook",
+          src: "/img/Storybook.png",
+        },
+        {
+          name: "Firebase",
+          src: "/img/Firebase.png",
+        },
+        {
+          name: "Styled-components",
+          src: "/img/Styled-components.png",
+        },
+        {
+          name: "HTML",
+          src: "/img/HTML.png",
+        },
+        {
+          name: "CSS",
+          src: "/img/CSS.png",
+        },
+        {
+          name: "Jest",
+          src: "/img/Jest.png",
+        },
+        {
+          name: "SASS",
+          src: "/img/SASS.png",
+        },
+        {
+          name: "Cypress.io",
+          src: "/img/Cypress.io.png",
+        },
+        {
+          name: "Bootstrap",
+          src: "/img/Bootstrap.png",
+        },
+        {
+          name: "JQuery",
+          src: "/img/JQuery.png",
+        },
+        {
+          name: "Prettier",
+          src: "/img/Prettier.png",
+        },
+        {
+          name: "ESLint",
+          src: "/img/ESLint.png",
+        },
+      ],
+    },
+    education: {
+      title: "Educação",
+      subtitle: "Estas são minhas qualificações",
+      faculty: "Uninassau",
+      course: "Analise e desenvolvimento de sistemas",
+      state: "Estado: Cursando",
+    },
+    project: {
+      title: "Project",
+      subtitle: "Look at some of my projects",
+      projectsCreated: [
+        {
+          name: "Login page next",
+          srcMobile: "/img/Project-Login-Page-Mobile.png",
+          srcDesktop: "/img/Project-Login-Page-Desktop.png",
+        },
+        {
+          name: "Pokedex",
+          srcMobile: "/img/Project-Pokedex-Mobile.png",
+          srcDesktop: "/img/Project-Pokedex-Desktop.png",
+        },
+        {
+          name: "Storybook",
+          srcMobile: "/img/Project-Storybook-Mobile.png",
+          srcDesktop: "/img/Project-Storybook-Desktop.png",
+        }
+      ]
+    },
+    footer: {
+      title: "Entrar em contato",
+      subtitle: "Qual é o próximo? Sinta-se à vontade para entrar em contato comigo se estiver procurando um desenvolvedor, tiver alguma dúvida ou simplesmente quiser se conectar.",
+      email: "ricardo.br.pi@gmail.com",
+      phone: "+55 86988278942",
+    }
+  },
+}
+
+export default locale;
