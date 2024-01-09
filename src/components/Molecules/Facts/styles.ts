5e311791c38d883cc771c83d2fdb1b6e4e8cbcc1
ricardo <ricardo.br.pi@gmail.com>
2024-01-09 16:58:17 -0300

    feat(facts): create facts component

---
+++ src/components/Molecules/Facts/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background: #282C33;
+  padding: 50px 0;
+
+`
+
+export const FactsStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+
+`;
+
+export const BoxText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 30px;
+`
+export const BoxFacts = styled.div`
+  display: flex;
+  padding: 0 50px;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+`
+export const BoxFact = styled.div`
+  border: 1px solid #ABB2BF;
+  padding: 10px;
+  margin: 8px;
+
+`
+
