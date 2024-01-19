b0fe987067e0e3fd6cdf6b27806b052698831e0c
ricardo <ricardo.br.pi@gmail.com>
2024-01-19 16:43:06 -0300

    feat(button): remove typing from uses and place fixed stylizations

--- src/components/Atoms/Button/index.tsx
+++ src/components/Atoms/Button/index.tsx
@@ -1,7 +1,7 @@
 import { ButtonProps } from "./types";
 import { ButtonStyle } from "./styles";
 
-export default function Button({ children, type, onClick, href, background, padding, borderRadius, color, border, fontSize, textDecoration, margin }: ButtonProps) {
+export default function Button({ children, type, onClick, href }: ButtonProps) {
   const buttonType = {
     type: type,
     onClick: onClick,
@@ -9,10 +9,9 @@ export default function Button({ children, type, onClick, href, background, padd
   const anchorType = {
     as: "a",
     href: href,
-    textDecoration: textDecoration,
   };
 
   return (
-    <ButtonStyle {...(href ? anchorType : buttonType)} background={background} padding={padding} borderRadius={borderRadius} color={color} border={border} textDecoration={textDecoration} fontSize={fontSize} margin={margin} >{children}</ButtonStyle>
+    <ButtonStyle {...(href ? anchorType : buttonType)} >{children}</ButtonStyle>
   );
 }
--- src/components/Atoms/Button/types.ts
+++ src/components/Atoms/Button/types.ts
@@ -6,12 +6,4 @@ export interface ButtonProps
   href?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined
   type?: "submit" | "reset" | "button";
-  background?: string;
-  padding?: string;
-  borderRadius?: string;
-  color?: string;
-  border?: string;
-  fontSize?: string;
-  textDecoration?: string;
-  margin?: string;
 }
