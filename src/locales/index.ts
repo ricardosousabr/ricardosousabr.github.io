3bf32c88f51b72e27722abcf5cc609a3bd13b986
ricardo <ricardo.br.pi@gmail.com>
2024-01-29 15:15:21 -0300

    feat: add data in English

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -126,6 +126,133 @@ const locale = {
       phone: "+55 (86) 9 8827-8942",
     }
   },
+  "en-us": {
+    header: {
+      title: "Hello, my name is Ricardo, I am a front-end developer",
+      subtitle: "I create responsive websites where technology meets creativity",
+      motovationalPhrase: "“The important thing is not to stop questioning; Curiosity has its own reason for existing” - Albert Einstein",
+      image: "/img/Profile.png",
+    },
+    skills: {
+      title: "Skills",
+      subtitle: "The skills, tools, and technologies I'm good at:",
+      technologies: [
+        {
+          name: "Next.js",
+          src: "/img/Next.js.png",
+        },
+        {
+          name: "React.js",
+          src: "/img/React.js.png",
+        },
+        {
+          name: "JavaScript",
+          src: "/img/JavaScript.png",
+        },
+        {
+          name: "Storybook",
+          src: "/img/Storybook.png",
+        },
+        {
+          name: "Firebase",
+          src: "/img/Firebase.png",
+        },
+        {
+          name: "Styled-components",
+          src: "/img/Styled-components.png",
+        },
+        {
+          name: "HTML",
+          src: "/img/HTML.png",
+        },
+        {
+          name: "CSS",
+          src: "/img/CSS.png",
+        },
+        {
+          name: "Jest",
+          src: "/img/Jest.png",
+        },
+        {
+          name: "SASS",
+          src: "/img/SASS.png",
+        },
+        {
+          name: "Cypress.io",
+          src: "/img/Cypress.io.png",
+        },
+        {
+          name: "Bootstrap",
+          src: "/img/Bootstrap.png",
+        },
+        {
+          name: "JQuery",
+          src: "/img/JQuery.png",
+        },
+        {
+          name: "Prettier",
+          src: "/img/Prettier.png",
+        },
+        {
+          name: "ESLint",
+          src: "/img/ESLint.png",
+        },
+      ],
+    },
+    facts: {
+      title: "My curiosities",
+      factsAbout: [
+        "I like winter more than summer",
+        "I often play with my friends",
+        "My favorite food is pizza and fries",
+        "My biggest dream is to travel all over the world",
+        "I love studying",
+        "Passionate about reading"
+      ]
+    },
+    education: {
+      title: "Education",
+      subtitle: "These are my qualifications",
+      faculty: "Uninassau",
+      course: "Analysis and systems development",
+      state: "Status: Studying",
+    },
+    project: {
+      title: "Projects",
+      subtitle: "See some of my projects",
+      projectsCreated: [
+        {
+          name: "Login page next",
+          link: "https://ricardosousabr.github.io/login-page-next/",
+          srcMobile: "/img/Project-Login-Page-Mobile.png",
+          srcDesktop: "/img/Project-Login-Page-Desktop.png",
+          technologies: ["JavaScript", "Next", "Styled-components", "Jest"],
+        },
+        {
+          name: "Pokedex",
+          link: "https://ricardosousabr.github.io/pokedex-react/",
+          srcMobile: "/img/Project-Pokedex-Mobile.png",
+          srcDesktop: "/img/Project-Pokedex-Desktop.png",
+          technologies: ["JavaScript", "Next", "Styled-components", "Jest"],
+
+        },
+        {
+          name: "Storybook",
+          link: "https://github.com/ricardosousabr/next-storybook",
+          srcMobile: "/img/Project-Storybook-Mobile.png",
+          srcDesktop: "/img/Project-Storybook-Desktop.png",
+          technologies: ["Storybook", "Next"],
+
+        }
+      ]
+    },
+    footer: {
+      title: "Get in touch",
+      subtitle: "What is the next? Feel free to contact me if you are looking for a developer, have any questions, or simply want to connect.",
+      email: "ricardo.br.pi@gmail.com",
+      phone: "+55 (86) 9 8827-8942",
+    }
+  },
 }
 
 export default locale;
