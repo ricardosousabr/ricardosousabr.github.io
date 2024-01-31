551c421904e97f5a7aa13844f8f4ea47aaf45a95
ricardo <ricardo.br.pi@gmail.com>
2024-01-31 16:07:04 -0300

    feat(header): add button to control language

--- src/components/Molecules/Header/styles.ts
+++ src/components/Molecules/Header/styles.ts
@@ -4,7 +4,6 @@ import screensYoga from "../../../tokens/screen";
 export const Wrapper = styled.div`
   background: #282C33;
   padding: 50px 0;
-
 `
 
 export const HeaderStyle = styled.div`
@@ -12,8 +11,8 @@ export const HeaderStyle = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`
 
-  `
 export const BoxInformation = styled.div`
   display: flex;
   flex-direction: column;
@@ -25,8 +24,8 @@ export const BoxInformation = styled.div`
     padding: 0 50px 100px 50px;
     flex-direction: row-reverse;
   }
-
 `
+
 export const BoxImage = styled.div`
   padding: 0 32px 32px 32px;
   flex-wrap: wrap;
@@ -36,8 +35,6 @@ export const BoxImage = styled.div`
     width: 400px;
     height: 500px;
   }
-
-
 `
 
 export const BoxTitle = styled.div`
@@ -60,9 +57,7 @@ export const BoxText = styled.div`
 
   @media (min-width: ${screensYoga.breakpointSm}) {
     padding: 0 60px 24px 0px;
-
   }
-
 `
 
 export const BoxButton = styled.div`
@@ -71,5 +66,29 @@ export const BoxButton = styled.div`
 export const BoxMessage = styled.div`
   border: 1px solid #ABB2BF;
   padding: 32px;
+`
 
+export const BoxSwitch = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+`
+
+export const Switch = styled.div<{enabled: boolean}>`
+  background: white;
+  width: 80px;
+  height: 40px;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+
+  `
+export const SwitchSpan = styled.span<{enabled: boolean}>`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: transparent;
+  margin: 5px;
+  transition-duration: 0.4s;
+  translate: ${props => props.enabled ? "0px" : "40px"};
 `
