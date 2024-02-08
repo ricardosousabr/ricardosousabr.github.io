d784f816b6d15f70dfe7733b044e4b42d59ab032
ricardo <ricardo.br.pi@gmail.com>
2024-02-08 10:26:48 -0300

    feat: change the main route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,12 @@
-import Header from "../src/components/Molecules/Header";
-import Skills from "../src/components/Molecules/Skills";
-import Facts from "../src/components/Molecules/Facts";
-import Education from "../src/components/Molecules/Education";
-import Project from "../src/components/Molecules/Project";
-import Footer from "../src/components/Molecules/Footer";
-import locale from "@/src/locales";
-import { NextSeo } from 'next-seo';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-export default function Home() {
-  const { header, skills, facts, education, project, footer } = locale["pt-br"];
+const IndexPage = () => {
+  const router = useRouter();
 
-  return (
-    <>
-      <NextSeo
-        title="Meu Portfolio - Ricardo"
-        description="Olá esse é o meu Portfolio venha, um desenvolvedor front-end apaixonado pela minha profissão. Me esforcei muito para aprender tudo que sei, veja mais sobre mim nesse link."
-        openGraph={{
-          type: 'website',
-          url: 'https://ricardosousabr.github.io/',
-          title: 'Acesse o link para conhecer sobre mim',
-          description: "Olá esse é o meu Portfolio venha, um desenvolvedor front-end apaixonado pela minha profissão. Me esforcei muito para aprender tudo que sei, veja mais sobre mim nesse link.",
-          images: [
-            {
-              url: '../public/img/Profile-SEO.png',
-              width: 800,
-              height: 600,
-              alt: 'Profile SEO',
-              type: 'image/png',
-            }
-          ],
-          siteName: 'Portfolio - Ricardo',
+  useEffect(() => {
+    router.push('/pt-br');
+  }, []);
+};
 
-        }}
-
-        additionalLinkTags={[
-          {
-            rel: 'icon',
-            href: '/icon/favicon.ico',
-          },
-          {
-            rel: 'preload',
-            href: 'https://fonts.googleapis.com/css2?family=Fira+Code&family=Oswald&family=Preahvihear&family=Roboto&display=swap',
-            as: 'font',
-            type: 'font/woff2',
-          },
-        ]}
-      />
-      <Header {...header} />
-      <Skills {...skills} />
-      <Facts {...facts} />
-      <Education {...education} />
-      <Project {...project} />
-      <Footer {...footer} />
-    </>
-  )
-}
+export default IndexPage;
